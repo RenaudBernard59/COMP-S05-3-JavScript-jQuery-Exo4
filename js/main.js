99cbda12650a06deab7518d81392e5b12b42762e
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-23 09:09:11 +0100

    Add files
    Adding files for starting project.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+CONSIGNE
+
+1/ A l'aide du plugin jQuery http://easyautocomplete.com/ :
+    - Afficher un champ de saisie qui affiche une liste de pays lorsque l'on tape des caractères
+    
+2/ A l'aide du plugin jQuery https://fengyuanchen.github.io/datepicker/ :
+    - Afficher un champ de saisie qui affiche un calendrier lorsque l'on clique dessus
+*/
