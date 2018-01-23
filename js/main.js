a30a5c21c90e6b3288fb0403813c3f0f459a0c0e
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-23 09:26:45 +0100

    Add component pays
    Adding files, link and code for pays autocomplete. But don't work.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,28 @@ CONSIGNE
 2/ A l'aide du plugin jQuery https://fengyuanchen.github.io/datepicker/ :
     - Afficher un champ de saisie qui affiche un calendrier lorsque l'on clique dessus
 */
+
+/*=====================Pays=====================*/
+
+var options = {
+
+  url: "resources/countries.json",
+
+  getValue: "name",
+
+  list: {	
+    match: {
+      enabled: true
+    }
+  },
+
+  theme: "square"
+};
+
+$("#countries").easyAutocomplete(options);
+
+
+/*=====================Date=====================*/
+
+
+
